0a680c51916b847c1239522e5bc27de0ec1f06a9
w2you <info@w2you.ru>
2021-04-27 12:40:09 +0300

    fix ftp

--- app/Widgets/views/regions/index.blade.php
+++ app/Widgets/views/regions/index.blade.php
@@ -1,13 +1,4 @@
 <div class="form-group">
-        @foreach($regions as $reg )
-            <a href="http://{{ $reg->slug }}.<?php echo env('SESSION_DOMAIN') ?>/">
-                @if($region->slug == $reg->slug)
-                    <b>
-                        {!!  $reg->title  !!}
-                    </b>
-                @else
-                    {!!  $reg->title  !!}
-                @endif
-            </a>
-        @endforeach
+       <a href="/"><b>Москва</b></a>
+	   <a href="https://www.prorabneva.ru"><b>Санкт-Петербург</b></a>
 </div>
